1c5518aa6382a7902656e648dde592f6cecb0f28
Michael Wagner <mdwranger@gmail.com>
2023-03-25 11:08:18 -0400

    fix: windows libc bindings

--- src/screen.cr
+++ src/screen.cr
@@ -1,12 +1,14 @@
 {% if flag?(:win32) %}
   # https://learn.microsoft.com/en-us/windows/console/clearing-the-screen#example-3
   # https://github.com/inancgumus/screen/blob/master/clear_windows.go
-  @[Link("kernel32")]
   lib LibC
     alias SHORT = Int16
-    alias WCHAR = UInt16
     alias TCHAR = WCHAR
     alias LPDWORD = DWORD*
+
+    STD_INPUT_HANDLE  = -10.to_u32!
+    STD_OUTPUT_HANDLE = -11.to_u32!
+    STD_ERROR_HANDLE  = -12.to_u32!
 
     struct COORD
       x, y : SHORT
@@ -15,6 +17,7 @@
     struct SMALL_RECT
       left, top, right, bottom : SHORT
     end
+
     type SmallRect = SMALL_RECT
 
     struct CONSOLE_SCREEN_BUFFER_INFO
@@ -23,47 +26,56 @@
       srWindow : SmallRect
       dwMaximumWindowSize : COORD
     end
+
     type ConsoleScreenBufferInfo = CONSOLE_SCREEN_BUFFER_INFO
 
     fun GetConsoleScreenBufferInfo(hConsoleOutput : HANDLE, lpConsoleScreenBufferInfo : ConsoleScreenBufferInfo*) : BOOL
-    fun SetConsoleCursorPosition(hConsoleOutput : HANDLE, dwCursorPosition : COORD*) : BOOL
+    fun SetConsoleCursorPosition(hConsoleOutput : HANDLE, dwCursorPosition : COORD) : BOOL
     fun FillConsoleOutputCharacterW(hConsoleOutput : HANDLE, cCharacter : TCHAR, nLength : DWORD, dwWriteCoord : COORD, lpNumberOfCharsWritten : LPDWORD) : BOOL
     fun FillConsoleOutputAttribute(hConsoleOutput : HANDLE, wAttribute : WORD, nLength : DWORD, dwWriteCoord : COORD, lpNumberOfAttrsWritten : LPDWORD) : BOOL
+    fun GetStdHandle(nStdHandle : DWORD) : HANDLE
   end
 {% end %}
 
 module Screen
   extend self
 
-  VERSION = "0.1.0"
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
+  # Clears the screen and resets cursor position
+  def cls : Nil
+    clear
+    move_top_left
+  end
+
+  # Clears the screen
   def clear : Nil
     {% if flag?(:win32) %}
-      handle = pointerof(STDOUT)
+      handle = LibC.GetStdHandle(LibC::STD_OUTPUT_HANDLE)
       cursor = LibC::COORD.new(x: 0, y: 0)
       h = get_screen
-      total : LibC::DWORD = h.value.dwSize.x * h.value.dwSize.y
-      LibC.FillConsoleOutputCharacterW(handle, ' ', total, cursor, out w)
-      LibC.FillConsoleOutputAttribute(handle, h.wAttributes, total, cursor, out w)
+      total = (h.dwSize.x * h.dwSize.y).to_u32
+      LibC.FillConsoleOutputCharacterW(handle, ' '.ord, total, cursor, out _)
+      LibC.FillConsoleOutputAttribute(handle, h.wAttributes, total, cursor, out _)
     {% else %}
       print "\033[2J"
     {% end %}
   end
 
+  # Resets cursor position
   def move_top_left : Nil
     {% if flag?(:win32) %}
-      handle = pointerof(STDOUT)
-      h = get_screen
-      coord = LibC::COORD.new(x: 0, y: 0)
-      LibC.SetConsoleCursorPosition(handle, coord)
+      handle = LibC.GetStdHandle(LibC::STD_OUTPUT_HANDLE)
+      cursor = LibC::COORD.new(x: 0, y: 0)
+      LibC.SetConsoleCursorPosition(handle, cursor)
     {% else %}
       print "\033[H"
     {% end %}
   end
 
   {% if flag?(:win32) %}
-    private def get_screen : LibC::ConsoleScreenBufferInfoType*
-      handle = pointerof(STDOUT)
+    private def get_screen : LibC::ConsoleScreenBufferInfo
+      handle = LibC.GetStdHandle(LibC::STD_OUTPUT_HANDLE)
       LibC.GetConsoleScreenBufferInfo(handle, out h)
       h
     end
