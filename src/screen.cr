77f4c5a1185e28bc1186c717e8e3b5710ff10abe
Michael Wagner <mdwranger@gmail.com>
2023-03-28 08:48:07 -0400

    feat: move lib inside Screen mod

--- src/screen.cr
+++ src/screen.cr
@@ -1,41 +1,41 @@
-{% if flag?(:win32) %}
-  lib LibC
-    alias SHORT = Int16
-    alias TCHAR = WCHAR
-
-    STD_INPUT_HANDLE  = -10.to_u32!
-    STD_OUTPUT_HANDLE = -11.to_u32!
-    STD_ERROR_HANDLE  = -12.to_u32!
-
-    struct COORD
-      x, y : SHORT
-    end
-
-    struct SMALL_RECT
-      left, top, right, bottom : SHORT
-    end
-
-    type SmallRect = SMALL_RECT
-
-    struct CONSOLE_SCREEN_BUFFER_INFO
-      dwSize, dwCursorPosition : COORD
-      wAttributes : WORD
-      srWindow : SmallRect
-      dwMaximumWindowSize : COORD
-    end
-
-    type ConsoleScreenBufferInfo = CONSOLE_SCREEN_BUFFER_INFO
-
-    fun GetConsoleScreenBufferInfo(hConsoleOutput : HANDLE, lpConsoleScreenBufferInfo : ConsoleScreenBufferInfo*) : BOOL
-    fun SetConsoleCursorPosition(hConsoleOutput : HANDLE, dwCursorPosition : COORD) : BOOL
-    fun FillConsoleOutputCharacterW(hConsoleOutput : HANDLE, cCharacter : TCHAR, nLength : DWORD, dwWriteCoord : COORD, lpNumberOfCharsWritten : DWORD*) : BOOL
-    fun FillConsoleOutputAttribute(hConsoleOutput : HANDLE, wAttribute : WORD, nLength : DWORD, dwWriteCoord : COORD, lpNumberOfAttrsWritten : DWORD*) : BOOL
-    fun GetStdHandle(nStdHandle : DWORD) : HANDLE
-  end
-{% end %}
-
 module Screen
   extend self
+
+  {% if flag?(:win32) %}
+    lib LibC
+      alias SHORT = Int16
+      alias TCHAR = WCHAR
+
+      STD_INPUT_HANDLE  = -10.to_u32!
+      STD_OUTPUT_HANDLE = -11.to_u32!
+      STD_ERROR_HANDLE  = -12.to_u32!
+
+      struct COORD
+        x, y : SHORT
+      end
+
+      struct SMALL_RECT
+        left, top, right, bottom : SHORT
+      end
+
+      type SmallRect = SMALL_RECT
+
+      struct CONSOLE_SCREEN_BUFFER_INFO
+        dwSize, dwCursorPosition : COORD
+        wAttributes : WORD
+        srWindow : SmallRect
+        dwMaximumWindowSize : COORD
+      end
+
+      type ConsoleScreenBufferInfo = CONSOLE_SCREEN_BUFFER_INFO
+
+      fun GetConsoleScreenBufferInfo(hConsoleOutput : HANDLE, lpConsoleScreenBufferInfo : ConsoleScreenBufferInfo*) : BOOL
+      fun SetConsoleCursorPosition(hConsoleOutput : HANDLE, dwCursorPosition : COORD) : BOOL
+      fun FillConsoleOutputCharacterW(hConsoleOutput : HANDLE, cCharacter : TCHAR, nLength : DWORD, dwWriteCoord : COORD, lpNumberOfCharsWritten : DWORD*) : BOOL
+      fun FillConsoleOutputAttribute(hConsoleOutput : HANDLE, wAttribute : WORD, nLength : DWORD, dwWriteCoord : COORD, lpNumberOfAttrsWritten : DWORD*) : BOOL
+      fun GetStdHandle(nStdHandle : DWORD) : HANDLE
+    end
+  {% end %}
 
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
 
